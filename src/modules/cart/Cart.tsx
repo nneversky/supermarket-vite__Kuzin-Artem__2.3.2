66340566dff2ccada13230de21bdbb912a4ec9e6
Artem <skeleton.ubica@gmail.com>
2025-07-16 23:44:09 +0300

    Update Cart.tsx

--- src/modules/cart/Cart.tsx
+++ src/modules/cart/Cart.tsx
@@ -1,37 +1,15 @@
 import "./Cart.css";
 import CartElement from "../../components/cartElement";
-import { PopupContext, ShowPopupContext } from "../../state/context";
+import { PopupContext } from "../../state/context";
 import { useContext, useState, useEffect, useRef } from "react";
 import { Image } from "@mantine/core";
 import emptyCart from "../../assets/image/emptyCart.svg";
 
 const Cart = () => {
   const { cart } = useContext(PopupContext);
-  const { showPopup, setShowPopup } = useContext(ShowPopupContext);
   const [total, setTotal] = useState(0);
   const [showImgemptyCart, setShowImgemptyCart] = useState(true);
   const popupRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        popupRef.current &&
-        !popupRef.current.contains(event.target as Node)
-      ) {
-        const headerButton = document.querySelector(".header__button");
-        headerButton?.classList.replace("cart--open", "cart--close");
-        setShowPopup(false);
-      }
-    };
-
-    if (showPopup) {
-      document.addEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [showPopup, setShowPopup]);
 
   useEffect(() => {
     if (cart.length !== 0) setShowImgemptyCart(false);
